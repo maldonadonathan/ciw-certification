c1b1d3bee470aaeed25ac8fea63cccc44fcf3146
xzthan <nmaldonado2725@gmail.com>
2022-04-25 23:44:46 -0700

    Labs 12

--- Lab_Files/L12 Lab Files/Lab12-2/script.js
+++ Lab_Files/L12 Lab Files/Lab12-2/script.js
@@ -1,20 +1,20 @@
 function slideSwitch() {
     var $active = $('#slideshow img.active');
 
-    if ( $active.length == 0 ) $active = $('#slideshow img:last');
+    if ($active.length == 0) $active = $('#slideshow img:last');
 
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow img:first');
+    var $next = $active.next().length ? $active.next() :
+        $('#slideshow img:first');
 
     $active.addClass('last-active');
 
-    $next.css({opacity: 0.0})
+    $next.css({ opacity: 0.0 })
         .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
+        .animate({ opacity: 1.0 }, 1000, function() {
             $active.removeClass('active last-active');
         });
 }
 
 $(function() {
-    setInterval( "slideSwitch()", 5000 );
+    setInterval("slideSwitch()", 5000);
 });
